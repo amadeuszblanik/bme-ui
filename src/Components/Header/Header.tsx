695f378d2036c38567917215fc8addc97718fc56
Amadeusz Blanik <amadeusz@blanik.me>
2020-10-01 01:37:34 +0200

    Changed Header italic

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { HeaderComponentProps } from "./Header.types";
 import { HeaderFontSize } from "./Header.variants";
 import {TextFontWeight} from "../Text/Text.variants";
+import {spacing} from "../../Mixins";
 
 const StyledHeader = styled.header<HeaderComponentProps>`
   width: 100%;
@@ -11,14 +12,14 @@ const StyledHeader = styled.header<HeaderComponentProps>`
   font-family: ${({ theme, fontFamily }) => theme.bme.fonts[fontFamily]};
   font-weight: ${TextFontWeight.bold};
   text-align: ${({ align }) => align};
-  font-style: italic;
 `;
 
 const StyledAccent = styled.span`
+  ${spacing('margin', { x: { right: "xs" } })}
   background: ${({ theme }) => `linear-gradient(45deg, ${theme.bme.palette.primary}, ${theme.bme.palette.secondary}, ${theme.bme.palette.tertiary})`};
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
-  font-style: normal;
+  font-style: italic;
 `;
 
 const Header: React.FunctionComponent<HeaderComponentProps> = ({ children, ...args }) => (
