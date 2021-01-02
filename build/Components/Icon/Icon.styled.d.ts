f34c56a07436f571c7b678a71ce6fd079a5c0164
Amadeusz Blanik <amadeusz@blanik.me>
2021-01-02 20:18:40 +0100

    Release 0.2.1 🚀

---
+++ build/Components/Icon/Icon.styled.d.ts
@@ -0,0 +1,6 @@
+import { Palette } from '../../Theme/Theme';
+export declare const GlobalIcon: import("styled-components").GlobalStyleComponent<{}, import("styled-components").DefaultTheme>;
+export declare const StyledIcon: import("styled-components").StyledComponent<"i", import("styled-components").DefaultTheme, {
+    icon: string | number;
+    variant?: Palette;
+}, never>;
