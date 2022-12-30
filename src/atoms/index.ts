7e82fdb3713ed85e962d9bd6750b2e6ea8d192dc
Amadeusz Blanik <amadeusz@blanik.me>
2022-12-30 21:55:28 +0100

    Added missing exports

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -1,5 +1,7 @@
+export { default as BmeAvatar } from "./avatar";
 export { default as BmeButton } from "./button";
 export { default as BmeCheckbox } from "./checkbox";
 export { default as BmeIcon } from "./icon";
 export { default as BmeInput } from "./input";
+export { default as BmeProgressBar } from "./progress-bar";
 export { default as BmeTextArea } from "./text-area";
